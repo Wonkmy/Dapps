61043c5931b3741c212caf7c28d3fcec9b05c316
qm994 <1589797819@qq.com>
2022-12-14 20:46:01 +0800

    first commit

---
+++ contract.js
@@ -0,0 +1,16 @@
+const contractaddress="0xe6cEA739882619b36898211b6F097030fB02e203";
+const contractAbi=[
+	{
+		"inputs": [],
+		"name": "greet",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "pure",
+		"type": "function"
+	}
+]
